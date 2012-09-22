3a00d9f75bbd65103d019cd862671597e0c36b9c
Eric Yu <ericfish@gmail.com>
2012-09-22 12:47:01 +0800

    change display, only for custome

--- gc-themes/default/about.php
+++ gc-themes/default/about.php
@@ -21,9 +21,6 @@
 
 	<div id="contentcenter">
 
-<div class="date">	
-		<img src="./<?php echo(TPPATH);?>/pic/sq.gif" width="7" height="7"> <?php get_blog_about_title(); ?>
-	</div>	
 
 <div class="blogbody">
 <p>
@@ -31,19 +28,8 @@
 </p>
 
 
-<div class="date">	
-		<img src="./<?php echo(TPPATH);?>/pic/sq.gif" width="7" height="7"> Site Stat
-	</div>	
 <p>
-	<p>
-	First post: <?php count_first_post_date(); ?><br/>
-	Last post: <?php count_last_post_date(); ?><br/>
-	Total days: <?php count_total_days(); ?><br/>
-	Total posts: <?php count_total_posts(); ?><br/>
-	Total comments: <?php count_total_comments(); ?><br/>
-	Posts/week: <?php count_posts_per_week(); ?><br/>
-	Comments/post: <?php count_comments_per_post(); ?>
-	</p><br/><br/><br/><br/>
+<br/><br/><br/><br/>
 </p>
 </div>
 
